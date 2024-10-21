8f07b30acc6bea20955ba02b90d6db2fea1ea3bf
Jhony Zac Rojas Escobar <ZacRojas@outlook.com>
2024-10-21 14:17:05 -0600

    se creo el componente TableSetData

---
+++ src/Components/Tables/TableSetData.jsx
@@ -0,0 +1,74 @@
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+
+const itemsPerPageOptions = [5, 10, 15]
+export const TableSetData = ({columns, page, setPage, rowsPerPage, 
+  setRowsPerPage, refetch, isLoading, data, setData}) => {
+  
+  const handleChangePage = (newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(1);
+    refetch();
+  };
+
+  if (isLoading) {
+    return <h1>reload</h1>
+  }
+  
+  if (!data?.items) {
+    return <h1>Sin Datos</h1>
+  }
+
+  return (
+    <div className="table-container">
+      <div className='table-content'>
+        <table>
+          <thead>
+            <tr>
+              {columns.map((column,index) => (
+                <th key={index}>{column.label}</th>
+              ))}
+              <th>Opciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.items.map((row) => {
+              return (
+                <tr key={row.id}>
+                  {columns.map((column, index) => {
+                    const value = column.value.split('.').reduce((o, i) => o[i], row);
+                    return (
+                      <td key={index} data-label={column.label}>
+                        {value}
+                      </td>)})}
+                  <td>
+                    <button onClick={()=>{setData(row)}}><CheckCircleIcon/></button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+      <div className="pagination">
+        <button className='paginationBTN' onClick={() => handleChangePage(page - 1)} disabled={page === 1}>
+          Previo
+        </button>
+        <span>Pagina {page} de {data.totalPages}</span>
+        <button className='paginationBTN' onClick={() => handleChangePage(page + 1)} disabled={page === data.totalPages}>
+          Next
+        </button>
+        <select className='select-items-per-page' value={rowsPerPage} onChange={handleChangeRowsPerPage}>
+          {itemsPerPageOptions.map((option) => (
+            <option key={option} value={option}>
+              {option} items por pagina
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  )
+}
