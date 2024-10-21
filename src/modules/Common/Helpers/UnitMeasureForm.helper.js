d0a69eabc1ef1e5f5f68082497b3a87733d7d34b
Jhony Zac Rojas Escobar <ZacRojas@outlook.com>
2024-10-21 12:42:09 -0600

    se agregaron las funciones helper par renderizar la tabla y los inputs para unitmeasuretable y unitmeasureform

--- src/modules/Common/Helpers/UnitMeasureForm.helper.js
+++ src/modules/Common/Helpers/UnitMeasureForm.helper.js
@@ -13,20 +13,29 @@ export const UnitMeasureFormfields = [
     type: "text",
     placeholder: "Ingrese el descripción de la unidad"
   },
+  {
+    name: "conversionfactor",
+    label: "Factor de conversión en gramos",
+    type: "number",
+    placeholder: "Ingrese el factor de conversion en gramos"
+  },
 ];
 
 export const initialValuesCreatedUnitMeasure = {
   name: "",
   description: "",
+  conversionfactor: "",
 };
 
 export const validationSchemaFormUnitMeasure = {
   name: Yup.string().required("Debe ingresar un nombre"),
   description: Yup.string().required("Debe ingresar una descripción"),
+  conversionfactor: Yup.number().required("Debe ingresar el factor de conversion en gramos"),
 };
 
 
 export const valuesUpdateUnitMeasure = (item) => ({
   name: item.name,
   description: item.description,
+  conversionfactor: item.conversionfactor,
 })
--- src/modules/Common/Helpers/UnitMeasureTable.helper.js
+++ src/modules/Common/Helpers/UnitMeasureTable.helper.js
@@ -1,4 +1,8 @@
 export const UnitMeasureColumns = [
   {label: "Unidad de medida", value: "name"},
   {label: "Descripción", value: "description"},
+  {label: "Factor de conversion", value: "conversionfactor"},
+  {label: "Fecha de creación:", value: "newcreatedAt"},
+  {label: "Fecha de actualización", value: "newupdatedAt"},
+  {label: "Estado", value: "estado"},
 ];
