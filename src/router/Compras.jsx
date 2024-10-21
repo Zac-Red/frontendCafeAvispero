9aa8b60d109615baf6b66c5a54fbb75fdca916ed
Jhony Zac Rojas Escobar <ZacRojas@outlook.com>
2024-10-21 11:42:46 -0600

    se agrego las rutas de compras y de reporte de materia prima

--- src/router/Compras.jsx
+++ src/router/Compras.jsx
@@ -1,9 +1,27 @@
 import DashboardIcon from '@mui/icons-material/Dashboard';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import AddBusinessIcon from '@mui/icons-material/AddBusiness';
+import SignalCellularAltIcon from '@mui/icons-material/SignalCellularAlt';
 
 export const comprasURLs = [
   {
     url: "/admin/compras/dashboard",
     name: "Dashboard",
     icon: <DashboardIcon/>
-  }
+  },
+  {
+    url: "/admin/compras/registrar",
+    name: "Registrar Compras",
+    icon: <AddShoppingCartIcon/>
+  },
+  {
+    url: "/admin/compras/compras",
+    name: "Compras",
+    icon: <AddBusinessIcon/>
+  },
+  {
+    url: "/admin/compras/reporte",
+    name: "Materia prima más comprada",
+    icon: <SignalCellularAltIcon/>
+  },
 ]
--- src/router/MateriaPrima.jsx
+++ src/router/MateriaPrima.jsx
@@ -1,6 +1,8 @@
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import HourglassBottomIcon from '@mui/icons-material/HourglassBottom';
 import AgricultureIcon from '@mui/icons-material/Agriculture';
+import InventoryIcon from '@mui/icons-material/Inventory';
+import SignalCellularAltIcon from '@mui/icons-material/SignalCellularAlt';
 
 export const materiaprimaURLs = [
   {
@@ -9,13 +11,28 @@ export const materiaprimaURLs = [
     icon: <DashboardIcon/>,
   },
   {
-    url: "/admin/materiaprima/materiaprima",
+    url: "/admin/materiaprima/materiasprimas",
     name: "Materia Prima",
     icon: <AgricultureIcon/>,
+  },
+  {
+    url: "/admin/materiaprima/tipos-movimientos",
+    name: "Tipos de Movimientos",
+    icon: <HourglassBottomIcon/>
   },
   {
     url: "/admin/materiaprima/unidades",
     name: "Unidades",
     icon: <HourglassBottomIcon/>,
+  },
+  {
+    url: "/admin/materiaprima/reporte",
+    name: "Materia prima más comprada",
+    icon: <SignalCellularAltIcon/>
+  },
+  {
+    url: "/admin/materiaprima/kardex",
+    name: "Kardex de materia prima",
+    icon: <InventoryIcon/>
   },
 ]
