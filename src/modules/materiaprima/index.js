bcbaabfa12b6a3d41b3dc6b4289b6f7e1bd5887c
Jhony Zac Rojas Escobar <ZacRojas@outlook.com>
2024-10-21 11:45:42 -0600

    se agregaron las importaciones del modulo de materia prima

--- src/modules/materiaprima/index.js
+++ src/modules/materiaprima/index.js
@@ -1,2 +1,4 @@
+export { KardexRawMaterial } from "./Pages/KardexRawMaterial/KardexRawMaterial";
 export { DashBoardRawMaterial } from "./Pages/DashBoardRawMaterial/DashBoardRawMaterial";
 export { ListRawMaterial } from "./Pages/ListRawMaterial/ListRawMaterial";
+
