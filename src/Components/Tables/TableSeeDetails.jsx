d72247d0c4acaf3dccfb29d72f4fdfbac9d62790
Jhony Zac Rojas Escobar <ZacRojas@outlook.com>
2024-10-21 14:16:42 -0600

    se creo el componente TableSeeDetails

---
+++ src/Components/Tables/TableSeeDetails.jsx
@@ -0,0 +1,42 @@
+import Avatar from '@mui/material/Avatar';
+import './TableData.css'
+
+export const TableSeeDetails = ({ data, columns, formatData, }) => {
+  if (!data) {
+    return <h1>Error</h1>
+  }
+  return (
+    <div className="table-container">
+      <div className='table-content'>
+        <table>
+          <thead>
+            <tr>
+              {columns.map((column, index) => (
+                <th key={index}>{column.label}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((row) => {
+              return (
+                <tr key={row.id}>
+                  {columns.map((column, index) => {
+                    const newFormat = formatData(row)
+                    const value = column.value.split('.').reduce((o, i) => o[i], newFormat);
+                    return (
+                      <td key={index} data-label={column.label}>
+                        {column.value === "url" ?
+                          <Avatar alt={value} src={value} />
+                          : value}
+                      </td>
+                    )
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
+}
