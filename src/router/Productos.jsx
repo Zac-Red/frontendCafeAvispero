57a72cfe7169752065023508b3ec9f69eeb87ba1
Jhony Zac Rojas Escobar <ZacRojas@outlook.com>
2024-10-21 11:18:01 -0600

    se agregaro las rutas de Tipos de Movimientos - Productos más vendidos - Kardex de producto

--- src/router/Productos.jsx
+++ src/router/Productos.jsx
@@ -1,6 +1,8 @@
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import CategoryIcon from '@mui/icons-material/Category';
 import HourglassBottomIcon from '@mui/icons-material/HourglassBottom';
+import InventoryIcon from '@mui/icons-material/Inventory';
+import SignalCellularAltIcon from '@mui/icons-material/SignalCellularAlt';
 
 export const productosURLs = [
   {
@@ -17,5 +19,20 @@ export const productosURLs = [
     url: "/admin/productos/unidades",
     name: "Unidades",
     icon: <HourglassBottomIcon/>
+  },
+  {
+    url: "/admin/productos/tipos-movimientos",
+    name: "Tipos de Movimientos",
+    icon: <HourglassBottomIcon/>
+  },
+  {
+    url: "/admin/productos/productos-top-ventas",
+    name: "Productos más vendidos",
+    icon: <SignalCellularAltIcon/>
+  },
+  {
+    url: "/admin/productos/kardex",
+    name: "Kardex de producto",
+    icon: <InventoryIcon/>
   },
 ]
