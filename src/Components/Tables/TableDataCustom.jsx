c79e7da44b9194374aa179094aa24ab3a3e949b2
Jhony Zac Rojas Escobar <ZacRojas@outlook.com>
2024-10-21 10:33:18 -0600

    se modificaron los botones par que sean opcionales dependiendo si existe la accion que realizaran

--- src/Components/Tables/TableDataCustom.jsx
+++ src/Components/Tables/TableDataCustom.jsx
@@ -1,13 +1,15 @@
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import Avatar from '@mui/material/Avatar';
 import './TableData.css'
 
 const itemsPerPageOptions = [5, 10, 20, 50];
-export const TableDataCustom = ({ columns, page, setPage, rowsPerPage, 
-    setRowsPerPage, updateFuntion, deleteFuntion, seeData, 
-    refetch, isLoading, data}) => {
-  
+export const TableDataCustom = ({ columns, formatData, page, setPage, rowsPerPage,
+  setRowsPerPage, updateFuntion, deleteFuntion, seeData, setData,
+  refetch, isLoading, data }) => {
+
   const handleChangePage = (newPage) => {
     setPage(newPage);
   };
@@ -21,7 +23,7 @@ export const TableDataCustom = ({ columns, page, setPage, rowsPerPage,
   if (isLoading) {
     return <h1>reload</h1>
   }
-  
+
   if (!data?.items) {
     return <h1>Error</h1>
   }
@@ -32,10 +34,10 @@ export const TableDataCustom = ({ columns, page, setPage, rowsPerPage,
         <table>
           <thead>
             <tr>
-              {columns.map((column,index) => (
+              {columns.map((column, index) => (
                 <th key={index}>{column.label}</th>
               ))}
-              <th>Opciones</th>
+              {(updateFuntion || deleteFuntion || seeData || setData) && <th>Opciones</th>}
             </tr>
           </thead>
           <tbody>
@@ -43,18 +45,28 @@ export const TableDataCustom = ({ columns, page, setPage, rowsPerPage,
               return (
                 <tr key={row.id}>
                   {columns.map((column, index) => {
-                    // console.log(new Date(row.updatedAt));
-                    const value = column.value.split('.').reduce((o, i) => o[i], row);
+                    const newFormat = formatData(row)
+                    const value = column.value.split('.').reduce((o, i) => o[i], newFormat);
                     return (
-                        <td key={index} data-label={column.label}>
-                          {value}
-                        </td>
-                      )})}
-                  <td>
-                    <button onClick={()=>{updateFuntion(row)}}><EditIcon/></button>
-                    <button onClick={()=>{deleteFuntion(row)}}><DeleteIcon/></button>
-                    <button onClick={()=>{seeData(row)}}><RemoveRedEyeIcon/></button>
-                  </td>
+                      <td key={index} data-label={column.label}>
+                        {column.value === "url" ?
+                          <Avatar alt={value} src={value} />
+                          : value}
+                      </td>
+                    )
+                  })}
+                  {(updateFuntion || deleteFuntion || seeData || setData) &&
+                    <td>
+                      {updateFuntion &&
+                        <button onClick={() => { updateFuntion(row) }}><EditIcon/></button>}
+                      {deleteFuntion &&
+                        <button onClick={() => { deleteFuntion(row) }}><DeleteIcon/></button>}
+                      {seeData &&
+                        <button onClick={() => { seeData(row) }}><RemoveRedEyeIcon/></button>}
+                      {setData &&
+                        <button onClick={() => { setData(row) }}><CheckCircleIcon/></button>}
+                    </td>
+                  }
                 </tr>
               );
             })}
@@ -62,16 +74,13 @@ export const TableDataCustom = ({ columns, page, setPage, rowsPerPage,
         </table>
       </div>
       <div className="pagination">
-        <button className='paginationBTN' onClick={() => handleChangePage( page - 1)} disabled={page === 1}>
+        <button className='paginationBTN' onClick={() => handleChangePage(page - 1)} disabled={page === 1}>
           Previo
         </button>
-
         <span>Pagina {page} de {data.totalPages}</span>
-
         <button className='paginationBTN' onClick={() => handleChangePage(page + 1)} disabled={page === data.totalPages}>
           Next
         </button>
-
         <select className='select-items-per-page' value={rowsPerPage} onChange={handleChangeRowsPerPage}>
           {itemsPerPageOptions.map((option) => (
             <option key={option} value={option}>
@@ -80,6 +89,6 @@ export const TableDataCustom = ({ columns, page, setPage, rowsPerPage,
           ))}
         </select>
       </div>
-  </div>
+    </div>
   );
 }
