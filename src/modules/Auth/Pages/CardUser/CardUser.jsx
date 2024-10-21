1ae42f404a08db11dea430e10d20f8c5426032c1
Jhony Zac Rojas Escobar <ZacRojas@outlook.com>
2024-10-21 11:24:37 -0600

    se agrego la ficha de usuario

--- src/modules/Auth/Pages/CardUser/CardUser.jsx
+++ src/modules/Auth/Pages/CardUser/CardUser.jsx
@@ -1,10 +1,10 @@
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
-import { fetchUnitData } from '../../../../Api/HttpServer.js';
+import { fetchByOneData } from '../../../../Api/HttpServer.js';
 import useAuthStore from '../../../../store/AuthStore.js';
-import Typography from '@mui/material/Typography';
-import './Carduser.css';
-import ProfielIMG from '/logo-coffe.png';
+import { formatUserForCard } from '../../utils/FormatUser.js';
+import { Card } from '../../../../Components/index.js';
+// import './Carduser.css';
 
 export const CardUser = () => {
   let { userId } = useParams();
@@ -12,40 +12,14 @@ export const CardUser = () => {
 
   const userSearch = useQuery({
     queryKey: ['userSearchTerm', {url:`/auth/${userId}`, token}],
-    queryFn: fetchUnitData, 
+    queryFn: fetchByOneData, 
   });
   if (!userSearch.data) return <h2>Sin datos</h2>
+  const Detail = formatUserForCard(userSearch.data);
+
   return (
-    <div className='containerCardUser'>
-      <div className='cardUser'>
-        <div> 
-          <Typography style={{ color: "orange"}} variant="h5" component="h3">Usuario</Typography>
-        </div>
-        <div className='profieldImage'>
-          <img src={ProfielIMG}/>
-        </div>
-        <Typography color={'white'} variant="h5" component="h3">
-          <span>Nombre: </span> 
-        {userSearch.data.firstname}</Typography>
-        <Typography color={'white'} variant="h5" component="h3">
-          <span>Apellido: </span> 
-          {userSearch.data.lastname}</Typography>
-        <Typography color={'white'} variant="h5" component="h3">
-          <span>DPI: </span> 
-          {userSearch.data.dpi}</Typography>
-        <Typography color={'white'} variant="h5" component="h3">
-          <span>Telefono: </span> 
-          {userSearch.data.phone}</Typography>
-        <Typography color={'white'} variant="h5" component="h3">
-          <span>Rol: </span> 
-          {userSearch.data.roleId.role}</Typography>
-        <Typography color={'white'} variant="h5" component="h3">
-          <span>Correo: </span> 
-          {userSearch.data.email}</Typography>
-        <Typography color={'white'} variant="h5" component="h3">
-          <span>Estado: </span> 
-          {userSearch.data.IsActive? "Activo":"Inactivo"}</Typography>
-      </div>
+    <div className='ContainerCustom'>
+      <Card title={"Datos del Usuario"} details={Detail}/>
     </div>
   )
 }
